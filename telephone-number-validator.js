b3443f06d699cef35da6511d65dc374a6fc6d1ce
Pankaj <pankajpj3103@gmail.com>
2023-10-30 14:27:55 +0530

    first commit

---
+++ telephone-number-validator.js
@@ -0,0 +1,9 @@
+function telephoneCheck(str) {
+    // Good luck!
+    let regex=/1{0,1}\s{0,1}((\(\d{3}\))|(\d{3}))[-]{0,1}[ ]{0,1}\d{3}[- ]{0,1}\d{4}/;
+     console.log(str.match(regex));
+   
+    return regex.test(str) && str.match(regex)[0]===str;
+   
+  }
+  telephoneCheck("555-555-5555");
